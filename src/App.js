33321f8acab4868ccedf70b330afd02d26cab15d
arianna choza <unachoza@gmail.com>
2020-02-20 14:57:04 -0500

    react router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Dashboard from './Pages/Dashboard/Dashboard.component';
 import Calculator from './Pages/Calculator/Calculator.component';
 import CategoryMatrix from './Pages/CategoryMatrix/CategoryMatrix.compoent';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
-      <Calculator />
-      <CategoryMatrix />
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/calculate" component={Calculator} />
+        <Route exact path="/matrix" component={CategoryMatrix} />
+      </Switch>
     </div>
   );
 }
