1f5e74935b3c1093eeeaf93a98805fee291c7bc8
Francine Altman <francinekline@gmail.com>
2020-02-20 17:27:29 -0500

    added image1

---
+++ src/Components/Rating/Rating.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Circle1 from '../img/Circle1.png'
+const Raiting = () => {
+  return (
+    <div className="raiting-container">
+      <img>src={Circle1}</img>
+    </div>
+  );
+};
+export default Raiting;
--- src/Pages/Dashboard/Dashboard.component.js
+++ src/Pages/Dashboard/Dashboard.component.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navigation from '../../Components/Navigation/Navigation.component';
 import OpenLeftMenu from '../../Components/OpenLeftMenu/OpenLeftMenu.component';
+import Circle1 from '../../img/Circle1.png'
 
 const Dashboard = () => {
   return (
     <div>
       <Navigation/>
-
+      <img src={Circle1}/>
       <div>Dashboard</div>
     </div>
   );
