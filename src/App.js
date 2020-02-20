51ceaa3ae2b2c981548f17da1a4ad967f279d49a
arianna choza <unachoza@gmail.com>
2020-02-20 14:50:06 -0500

    pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Dashboard from './Pages/Dashboard/Dashboard.component';
+import Calculator from './Pages/Calculator/Calculator.component';
+import CategoryMatrix from './Pages/CategoryMatrix/CategoryMatrix.compoent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> agoog
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard />
+      <Calculator />
+      <CategoryMatrix />
     </div>
   );
 }
---
+++ src/Pages/CategoryMatrix/CategoryMatrix.compoent.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const CategoryMatrix = () => <div>Category Matrix</div>
+
+export default CategoryMatrix
